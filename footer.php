50f8d06bf28e2de7299fa87d8f3e74e453a74c10
Jihan Aferiansyah <49224537+jihanaferiansyah@users.noreply.github.com>
2020-12-31 18:52:30 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span><strong>Copyright &copy;</strong>  <?= date('Y')?> , Sarjana IT</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
